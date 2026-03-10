a77bc0368597e7e07efdfa2bf88bbedab04e206b
Someshwar Patel <someshwarpatelm@gmail.com>
2026-03-10 15:05:36 +0530

    updated

---
+++ src/App.jsx
@@ -0,0 +1,14 @@
+import Header from './components/Header';
+import Content from './components/content';
+import Footer from './components/Footer';
+function App() {
+ 
+  return  ( <div><Header />
+  <Content />
+  <Footer />
+  </div>
+  );
+
+}
+
+export default App;
